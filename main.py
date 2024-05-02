3511e9400cd5bcd6fa0ff0a2a982678ddf8b1c15
TimaSv-Uk <sviridenkotimofei@gmail.com>
2024-05-02 21:29:52 +0300

    finish

--- main.py
+++ main.py
@@ -12,59 +12,45 @@ for line in f:
     weight = int(line.split(" ")[2])
     G.add_edge(node1, node2, weight=weight)
 f.close()
+
 # 3. Вивести визначення та значення основних характеристик графа (матриця суміжності, інциндентності, хроматичне число та реберне хроматичне число та інші)
-print("\nMатриця суміжності\n", nx.adjacency_matrix(G))
-print("\nMатриця інциндентносi\n", nx.incidence_matrix(G))
-# Compute the chromatic number
+print("\nMатриця суміжності:\n", nx.adjacency_matrix(G))
+print("\nMатриця інциндентносi:\n", nx.incidence_matrix(G))
+
 graph_coloring = nx.coloring.greedy_color(G)
 unique_colors = set(graph_coloring.values())
-# Identify edges with different colors
-chromatic_edges = [
-    (u, v) for u, v in G.edges() if graph_coloring[u] != graph_coloring[v]
-]
 
-print(f"\nХроматичне число: {len(unique_colors)}")
-# Calculate the maximum degree of the graph
 max_degree = max(G.degree, key=lambda x: x[1])[1]
 
-# The edge chromatic number is either max_degree or max_degree + 1
 edge_chromatic_number = max_degree if nx.is_eulerian(G) else max_degree + 1
-
+print(f"\nХроматичне число: {len(unique_colors)}")
 print(f"Реберне хроматичне число: {edge_chromatic_number}")
-print(f"\nChromatic edges: {chromatic_edges}")
 
 # 4. Написати програму для знаходження всіх ейлерових ланцюгів та циклів
 if nx.is_eulerian(G):
     eulerian_circuit_edges = list(nx.eulerian_circuit(G))
-    print("Eulerian circuit edges:", eulerian_circuit_edges)
-
-    eulerian_circuit_vertices = [u for u, v in eulerian_circuit_edges]
-    print("Eulerian circuit vertices:", eulerian_circuit_vertices)
+    print("Ребра ейлерового циклу:", eulerian_circuit_edges)
 else:
     print("\nГраф не є ейлеровим")
     H = nx.eulerize(G)
 
     eulerian_circuit_edges = list(nx.eulerian_circuit(H))
-    print("Eulerian circuit edges:", eulerian_circuit_edges)
-
-    # Get the sequence of vertices in the Eulerian circuit
-    eulerian_circuit_vertices = [u for u, v in eulerian_circuit_edges]
-    print("Eulerian circuit vertices:", eulerian_circuit_vertices)
+    print("Ребра ейлерового циклу:", eulerian_circuit_edges)
 if nx.has_eulerian_path(G):
-    eulerian_path_edges = list(nx.eulerian_path(G))
-    print("Eulerian path edges:", eulerian_path_edges)
+    eulerian_path_edges = list(nx.eulerian_path(H))
+    print("Ребра ейлерового шляху:", eulerian_path_edges, "\n")
 else:
-    print("The graph does not have an Eulerian path.")
+    print("Граф не має ейлерового шляху.")
     H = nx.eulerize(G)
 
-    print("Original grahp:")
+    print("Оригінальний граф:")
     print(nx.edges(G))
 
-    print("\nNew eulerian grahp:")
+    print("\nНовий ейлерів граф:")
     print(nx.edges(H))
 
     eulerian_path_edges = list(nx.eulerian_path(H))
-    print("Eulerian path edges:", eulerian_path_edges)
+    print("Ребра ейлерового шляху:", eulerian_path_edges, "\n")
 
 
 # 5. Написати програму для знаходження всіх гамільтонових ланцюгів та циклів
@@ -72,8 +58,8 @@ def hamiltonianPaths(G, v, visited, path):
     # if all the vertices are visited, then the Hamiltonian path exists
     if len(path) == len(G.nodes):
         if path[0] in G.neighbors(path[-1]):
-            print("Hamiltonian cycle found:", path + [path[0]])
-            print("Hamilton graph")
+            print("Гамільтоновий цикл:", path + [path[0]])
+            print("Граф є гамільтоновим")
         print(path)
         return
 
@@ -88,6 +74,7 @@ def hamiltonianPaths(G, v, visited, path):
 
 
 def findHamiltonianPaths(G):
+    print("\nГамільтоновиі ланцюги:")
     for start in G.nodes:
         path = [start]
         visited = {node: False for node in G.nodes}
@@ -101,21 +88,21 @@ findHamiltonianPaths(G)
 
 # 6. Реалізувати програмно обхід графа пошуком углиб
 dfs_edges = list(nx.dfs_edges(G, source=list(G.nodes)[0]))
-print("\nобхід графа пошуком углиб:", dfs_edges)
+print("\nОбхід графа пошуком углиб:", dfs_edges)
 
 # 7. Реалізувати програмно обхід графа пошуком вшир
 dfs_edges = list(nx.bfs_edges(G, source=list(G.nodes)[0]))
-print("\nобхід графа пошуком вшир", dfs_edges)
+print("\nОбхід графа пошуком вшир:", dfs_edges)
 
 
 # 8. На мові програмування реалізувани пошук найкородших відстаней на графі від заданої вершини (алгоритм Дейкстри). Вивести схему маршрутів і довжину відстаней.
 path = dict(nx.all_pairs_dijkstra_path(G), weight="weight")
-print("\nалгоритм Дейкстри:", path)
+print("\nАлгоритм Дейкстри:", path)
 
 # 9. На мові програмування реалізувани пошук найкородших відстаней між будь-якими двома вершинами графу (алгоритм Флойда). Вивести схему маршрутів і довжину відстаней.
 fw = nx.floyd_warshall(G, weight="weight")
 results = {a: dict(b) for a, b in fw.items()}
-print("\nалгоритм Флойда:", results)
+print("\nАлгоритм Флойда:", results)
 
 # 10. Знайти між якою парою вершин найкоротша відстань є найдовшою серед всіх найкоротших відстаней.
 # results
@@ -127,7 +114,7 @@ for start_node, connections in results.items():
             max_distance = distance
             vertices = (start_node, end_node)
 print(
-    f"\nнайкоротша відстань є найдовшою серед всіх найкоротших відстаней: {vertices}, {max_distance}."
+    f"\nНайкоротша відстань є найдовшою серед всіх найкоротших відстаней: {vertices}, {max_distance}."
 )
 
 # 11. Знайти між якою парою вершин найкоротша відстань є найкоротшою серед всіх найкоротших відстаней.
@@ -139,7 +126,7 @@ for start_node, connections in results.items():
             min_distance = distance
             vertices = (start_node, end_node)
 print(
-    f"\nнайкоротша відстань є найкоротшою серед всіх найкоротших відстаней: {vertices}, {min_distance}."
+    f"\nНайкоротша відстань є найкоротшою серед всіх найкоротших відстаней: {vertices}, {min_distance}."
 )
 
 graph_color_to_mpl_color = dict(zip(unique_colors, mpl.TABLEAU_COLORS))
